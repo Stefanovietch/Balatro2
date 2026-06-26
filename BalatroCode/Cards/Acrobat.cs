66b4032d4e1b4e6fe5295223f9e59d8136eee401
Stefanovietch <72559955+Stefanovietch@users.noreply.github.com>
2026-06-26 20:45:37 +0200

    commit 4

--- BalatroCode/Cards/Acrobat.cs
+++ BalatroCode/Cards/Acrobat.cs
@@ -5,8 +5,10 @@ using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Balatro.BalatroCode.Cards;
@@ -17,14 +19,13 @@ public class Acrobat() : BalatroCard(1,
     TargetType.AnyEnemy)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new DamageVar(9, ValueProp.Move),
+        new DamageVar(9, ValueProp.Move)
     ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        if (PileType.Hand.GetPile(this.Owner).IsEmpty) ModifyDamageMultiplicative(play.Target,3,ValueProp.Move,this.Owner.Creature, this);
         ArgumentNullException.ThrowIfNull(play.Target);
         await DamageCmd.Attack(this.DynamicVars.Damage.BaseValue).FromCard(this).Targeting(play.Target).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
     }
@@ -33,4 +34,13 @@ public class Acrobat() : BalatroCard(1,
     {
         this.DynamicVars.Damage.UpgradeValueBy(3);
     }
+
+    public override decimal ModifyDamageMultiplicative(Creature? target, decimal amount, ValueProp props, Creature? dealer,
+        CardModel? cardSource)
+    {
+        if (PileType.Hand.GetPile(this.Owner).Cards.Count <= 1) return amount * 3;
+        return amount;
+    }
+
+    protected override bool ShouldGlowGoldInternal => PileType.Hand.GetPile(this.Owner).Cards.Count <= 1;
 }
